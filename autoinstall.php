9a066f878956f45c4843f1a6e367f23327901d59
Tom Homer <tomhomer@gmail.com>
2018-09-28 10:54:16 -0400

    Updated Version to 1.1.3 and Fixed undeclared variable issue
    Fixed issue #8

--- autoinstall.php
+++ autoinstall.php
@@ -22,7 +22,7 @@ function plugin_autoinstall_autotags($pi_name)
     $info = array(
         'pi_name'         => $pi_name,
         'pi_display_name' => $pi_display_name,
-        'pi_version'      => '1.1.2',
+        'pi_version'      => '1.1.3',
         'pi_gl_version'   => '2.1.3',
         'pi_homepage'     => 'https://github.com/Geeklog-Plugins/autotags'
     );
